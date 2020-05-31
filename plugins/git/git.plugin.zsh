4c4145ff548d383e4a719686bcbd43bf68d46a17
joest <chaojiong.zheng@gmail.com>
2020-05-31 12:25:03 +0800

    Revert "update git shorcuts"
    This reverts commit ac2b94a19745b7fd95b1d74f494ae495001ebf76.

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -227,8 +227,7 @@ alias gpf='git push --force-with-lease'
 alias gpf!='git push --force'
 alias gpoat='git push origin --all && git push origin --tags'
 compdef _git gpoat=git-push
-alias gppu='git push upstream $(current_branch)'
-alias gpu='git pull upstream $(current_branch)'
+alias gpu='git push upstream'
 alias gpv='git push -v'
 
 alias gr='git remote'
